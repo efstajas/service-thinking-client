ac4504d70db801a18ef917082f7b5e1e0b12fbf6
Jason Efstathiou <jason.efst@gmail.com>
2019-04-27 21:57:37 +0200

    fix nav link

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -3,7 +3,7 @@
         <div class="inner">
             <div class="chapters">
                 <div class="chapter" :class="{active: $route.params.slug === chapter.slug}" v-for="(chapter) in $store.state.chapters" :key="chapter.title">
-                    <router-link class="link" @click.native="handleLinkClick" :to="chapter.slug">
+                    <router-link class="link" @click.native="handleLinkClick" :to="`/chapter/${chapter.slug}`">
                         <span class="title">{{ chapter.info.title }}</span>
                         <scrollactive v-if="$route.params.slug === chapter.slug" class="subchapters">
                             <a :href="subchapter.link" class="scrollactive-item" v-for="(subchapter, index) in $store.state.currentSubchapters" :key="index">{{ subchapter.title }}</a>
